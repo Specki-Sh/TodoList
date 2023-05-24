57e597b54200afeeba306887312662ccd7e5dc57
Specki <yushakhzod@gmail.com>
2023-05-24 07:23:30 +0300

    modify: new methods

--- domain/model/storage.go
+++ domain/model/storage.go
@@ -1,9 +1,19 @@
 package model
 
-type Storage interface {
-	Add(item Task) (int, error)
-	Remove(id int) error
-	GetByID(id int) (Task, error)
-	GetAll() ([]Task, error)
+type TaskStorage interface {
+	Create(item Task) (int, error)
+	Delete(id int) error
+	SelectByID(id int) (Task, error)
+	SelectAll() ([]Task, error)
 	Update(item Task) error
+	SelectAllCompleted() ([]Task, error)
+	MarkAllComplete() error
+}
+
+type UserStorage interface {
+	Create(item User) (int, error)
+	Update(item User) error
+	Delete(id int) error
+	SelectByID(id int) (User, error)
+	SelectAll() ([]User, error)
 }
