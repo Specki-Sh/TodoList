34c1ac0785a13c215f03636649a5422d142c4986
Specki <yushakhzod@gmail.com>
2023-05-24 07:24:48 +0300

    init: handerls for tasks

---
+++ handlers/task_handlers.go
@@ -0,0 +1,147 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+	"todolist/controller"
+	"todolist/domain/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewTaskHandler(taskController *controller.TaskController) *TaskHandlers {
+	return &TaskHandlers{taskController: taskController}
+}
+
+type TaskHandlers struct {
+	taskController *controller.TaskController
+}
+
+func (t *TaskHandlers) MarkAllComplete(c *gin.Context) {
+	err := t.taskController.MarkAllComplete()
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Success",
+	})
+}
+
+func (t *TaskHandlers) MarkComplete(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"reason": "invalid product id",
+		})
+		return
+	}
+
+	if err := t.taskController.MarkComplete(id); err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Success",
+	})
+}
+
+func (t *TaskHandlers) MarkNotComplete(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"reason": "invalid product id",
+		})
+		return
+	}
+
+	if err := t.taskController.MarkNotComplate(id); err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Success",
+	})
+}
+
+func (t *TaskHandlers) Delete(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"reason": "invalid product id",
+		})
+		return
+	}
+
+	if err := t.taskController.Remove(id); err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Success",
+	})
+}
+
+func (t *TaskHandlers) GetAll(c *gin.Context) {
+	allTasks, err := t.taskController.ShowAll()
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, allTasks)
+}
+
+func (t *TaskHandlers) GetCompleted(c *gin.Context) {
+	doneTasks, err := t.taskController.ShowCompleted()
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, doneTasks)
+}
+
+func (t *TaskHandlers) Create(c *gin.Context) {
+	var task model.Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	id, err := t.taskController.AddTask(task)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"id": id})
+}
+
+func (t *TaskHandlers) GetByID(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	task, err := t.taskController.Show(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, task)
+}
+
+func (t *TaskHandlers) PatchCompeteStatus(c *gin.Context) {
+	complete := c.Query("complete")
+	if complete == "true" {
+		t.MarkComplete(c)
+	} else if complete == "false" {
+		t.MarkNotComplete(c)
+	} else {
+		c.String(http.StatusBadRequest, "Missing or invalid 'complete' query parameter")
+	}
+}
