2a5e044f566c88bbd664cff91196c18b501f7916
Specki <yushakhzod@gmail.com>
2023-05-24 07:25:05 +0300

    init: handerls for users

---
+++ handlers/user_handlers.go
@@ -0,0 +1,87 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+	"todolist/controller"
+	"todolist/domain/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewUserHandlers(userController *controller.UserController) *UserHandlers {
+	return &UserHandlers{userController: userController}
+}
+
+type UserHandlers struct {
+	userController *controller.UserController
+}
+
+func (u *UserHandlers) Create(c *gin.Context) {
+	var user model.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	id, err := u.userController.Add(user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"id": id})
+}
+
+func (u *UserHandlers) Update(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	var user model.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user.ID = id
+	if err := u.userController.Edit(user); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.Status(http.StatusOK)
+}
+
+func (u *UserHandlers) Delete(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := u.userController.Remove(id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.Status(http.StatusOK)
+}
+
+func (u *UserHandlers) GetByID(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user, err := u.userController.Show(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func (u *UserHandlers) GetAll(c *gin.Context) {
+	users, err := u.userController.ShowAll()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, users)
+}
