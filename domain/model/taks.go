c7ce6dd8fb65df8d7515e4ce49e3f7c1395195ef
Specki <yushakhzod@gmail.com>
2023-05-24 04:06:20 +0300

    update: correct id type string->int

--- domain/model/taks.go
+++ domain/model/taks.go
@@ -3,7 +3,7 @@ package model
 import "time"
 
 type Task struct {
-	ID          string    `json:"id"`
+	ID          int    `json:"id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
 	DueDate     time.Time `json:"due_date"`
