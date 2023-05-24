c2edf22a3e381d84c977df2b263202b631f9fff5
Specki <yushakhzod@gmail.com>
2023-05-24 07:24:19 +0300

    formated: added space

--- domain/model/taks.go
+++ domain/model/taks.go
@@ -3,7 +3,8 @@ package model
 import "time"
 
 type Task struct {
-	ID          int    `json:"id"`
+	ID          int       `json:"id"`
+	UserID      int       `json:"user_id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
 	DueDate     time.Time `json:"due_date"`
