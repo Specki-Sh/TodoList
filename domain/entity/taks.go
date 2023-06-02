056720001eb29dbc9dd8334d9288802acdd771f8
Specki <yushakhzod@gmail.com>
2023-06-02 10:30:37 +0300

    modify: rename domain model -> entity

--- domain/entity/taks.go
+++ domain/entity/taks.go
@@ -3,11 +3,11 @@ package entity
 import "time"
 
 type Task struct {
-	ID          int       `json:"id"`
-	UserID      int       `json:"user_id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	DueDate     time.Time `json:"due_date"`
-	Priority    int       `json:"priority"`
-	Completed   bool      `json:"completed"`
+	ID          int        `json:"id"`
+	UserID      int        `json:"user_id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	DueDate     *time.Time `json:"due_date"`
+	Priority    int        `json:"priority"`
+	Completed   bool       `json:"completed"`
 }
